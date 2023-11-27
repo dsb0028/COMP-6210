461c8552713f63c3eae908350c05daecc41e066a
Daniel Benjamin <dbenj711@yahoo.com>
2023-11-27 00:01:30 -0500

    Checkpoint 4.97

--- ThreeAddressCode.py
+++ ThreeAddressCode.py
@@ -43,18 +43,20 @@ class ThreeAddressCode:
 threeAddressCodeDict = defaultdict(list)
 og_variable = None
 statement_type = None
+"""
 parentNode  = None
 branch1 = None
 branch2 = None
 left_branch_temp = None
 right_branch_temp = None
-
+"""
 def createThreeAddressCode(astTree, symbolTable):
     function_name = 'main'
     statementList = astTree[function_name]['Statement']
     for statement in statementList: 
         global visited_elements
         visited_elements = []
+        """
         global parentNode
         parentNode = None
         global left_branch_temp
@@ -64,6 +66,7 @@ def createThreeAddressCode(astTree, symbolTable):
         global branch1,branch2
         branch1 = None
         branch2 = None
+        """
         if '=' in statement:
             global statement_type
             statement_type = 'Assignment_Statement'
@@ -88,11 +91,97 @@ def createThreeAddressCode(astTree, symbolTable):
     return threeAddressCodeDict
 
 
-
 visited_elements = []
-temps = []
+temp_dicts = {}
 visited = None
+l_temp = None
+r_temp = None
+temp_root = None
 i = 1
+
+def walk_through_ast(root):
+    internal_nodes = ['+','-','*','/']
+    if root:
+        root_key = list(root.keys())[0]
+        if root_key in internal_nodes:
+            children = root[root_key]
+            #check the types of the children
+            left = children[0]
+            breakpoint()
+            walk_through_ast(left)
+            breakpoint()
+            #visited = visited_elements
+            #print(visited)
+            if len(children) == 2:
+                right = children[1]
+            
+            visited_elements.append(root_key)
+            if left != None and right != None:
+                global i
+                breakpoint()
+                m = [item for item in temp_dicts.items() if item[1] == left]
+                if list(left.keys())[0] in internal_nodes:
+                    if m == []:
+                        breakpoint()
+                        temp_dicts.update({"t"+str(i):left})
+                        root[root_key][0] = "t"+str(i)
+                        left = root[root_key][0]
+                        i = i + 1
+
+                m1 = [item for item in temp_dicts.items() if item[1] == right]
+                if list(right.keys())[0] in internal_nodes:
+                    if m1 == []:
+                        breakpoint()
+                        temp_dicts.update({"t"+str(i):right})
+                        root[root_key][1] = "t"+str(i)
+                        right = root[root_key][1]
+                        i = i + 1
+                """
+                if temp_dicts != {}:
+                    for key, val in temp_dicts.items():
+                        breakpoint()
+                        if list(left.keys())[0] in internal_nodes:
+                            if temp_dicts[key] == left:
+                                root[root_key][0] = key
+                                left = root[root_key][0]
+                        breakpoint()
+                        if list(right.keys())[0] in internal_nodes:
+                            m = [item for item in temp_dicts.items() if item[1] == right]
+                            if m == []:
+                                temp_dicts.update({"t"+str(i):right})
+                                i = i+1
+                            
+                        
+                """     
+                breakpoint()
+                print(left,right,root_key)
+                print(root)
+            walk_through_ast(right)
+            #print(visited_elements)
+           
+        else:
+            breakpoint()
+            visited_elements.append(root[root_key])
+            """
+            global i
+            if len(visited_elements) == 3:
+                temps.append( "t" + str(i))
+                threeAddressCode  = ThreeAddressCode(visited_elements[1],
+                                              visited_elements[0],
+                                              visited_elements[2],
+                                              "t" + str(i),statement_type)
+                threeAddressCodeDict['Three_Address_Code'].append(threeAddressCode)
+                                
+                visited_elements[-3] = "t" + str(i)
+                visited_elements.pop(-2)
+                visited_elements.pop(-1)             
+                i = i + 1
+            """
+    pass
+
+
+
+"""
 def walk_through_ast(root):
     left = None
     right = None
@@ -160,13 +249,8 @@ def walk_through_ast(root):
                                               right_branch_temp,
                                               og_variable,statement_type)
         threeAddressCodeDict['Three_Address_Code'].append(threeAddressCode)
-    """
-    else:
-        #return threeAddressCodeDict
-        breakpoint()
-        pass
-    """
-pass
+
+"""
 """
 def walk_through_ast(astSubTree,left=None,right=None):
     breakpoint()
